884e399b5cb3c869c35fc87dd219e6bda766a857
Joan Mon <joan.mon@socialpoint.es>
2017-10-12 23:59:06 +0200

    Add mongo

--- chatbot.py
+++ chatbot.py
@@ -3,5 +3,13 @@ from chatterbot import ChatBot
 def bot():
     return ChatBot(
         'VilaNetBot',
-        storage_adapter='chatterbot.storage.SQLStorageAdapter',
+        storage_adapter='chatterbot.storage.MongoDatabaseAdapter',
+        logic_adapters=[
+            'chatterbot.logic.BestMatch'
+        ],
+        filters=[
+            'chatterbot.filters.RepetitiveResponseFilter'
+        ],
+        database='chatterbot-database',
+        database_uri='mongodb://mongo:27017/'
     )
